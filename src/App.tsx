4b9d88fd2fb75e1a1616ffe46ea6c033511ee903
Bluemo <shutaro.aoyama@gmail.com>
2022-02-18 23:53:08 +0900

    Install draft.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {convertFromRaw, DraftEditorCommand, DraftHandleValue, Editor, EditorState, RichUtils} from "draft-js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [editorState, setEditorState] = React.useState(EditorState.createEmpty());
+
+    return (
+        <>
+            <Editor
+                editorState={editorState}
+                onChange={setEditorState}
+            />
+        </>
+    );
 }
 
 export default App;
